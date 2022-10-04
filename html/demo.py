76ec53d08746897b05d8ae0b91dd8849e051d06b
mohan2020coder <62936388+mohan2020coder@users.noreply.github.com>
2022-10-04 22:24:28 +0530

    Add files via upload

---
+++ html/demo.py
@@ -0,0 +1,14 @@
+print("hello")
+
+#from pythondemo import student
+from pythondemo import sum
+
+print(sum.add(10,2))
+print(sum.display("mohan"))
+
+#student_dict = {'name' : 'John', 'gender': 'Male', 'year': '3'}
+
+#stu = student.Student(student_dict)
+
+
+#print(stu.get_student_details())
